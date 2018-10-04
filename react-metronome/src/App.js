33bc2c54ba060082fd15be2b101c9742a8f2acfa
Handrew <andrew.a.han@gmail.com>
2018-10-03 23:20:27 -0400

    download audio click sounds and import to App.js

--- react-metronome/src/App.js
+++ react-metronome/src/App.js
@@ -1,16 +1,41 @@
 import React, { Component } from 'react';
 import './App.css';
+import click1 from './audio/click1.wav';
+import click2 from './audio/click2.wav';
 
 class Metronome extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      playing: false,
+      count: 0,
+      bpm: 100,
+      beatsPerMeasure: 4,
+    }
+  }
+
+  handleBpmChange = event => {
+    const bpm = event.target.value;
+    this.setState({
+      bpm
+    });
+  }
+
   render() {
-    let bpm = 100;
-    let playing = false;
+    const { playing, bpm } = this.state;
 
     return (
       <div className="App">
         <div className="bpm-slider">
           <div>{bpm} BPM</div>
-          <input type="range" min="60" max="240" value={bpm} />
+          <input
+            type="range"
+            min="60"
+            max="240"
+            value={bpm} 
+            onChange={this.handleBpmChange}
+          />
         </div>
         <button>{playing ? 'Playing' : 'Stop'}</button>
       </div>
